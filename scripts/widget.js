871eb86f7f89af4f33d27127eba3a609dfa74d83
KuzAnton8335 <ant2783@gmail.com>
2023-06-20 16:00:55 +0300

    start project

---
+++ scripts/widget.js
@@ -0,0 +1,13 @@
+import { startWidget } from "./modules/widgetServices.js";
+
+const initWidget = (app) => {
+
+	const widget = startWidget();
+
+	app.append(widget);
+
+}
+
+
+
+initWidget(document.querySelector('#app'));
